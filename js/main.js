ece6fba7dcea5d41978843c162c7479ede0a076a
mrp-afl <mrp.github@gmail.com>
2016-05-16 19:53:08 +1000

    Comments in main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+// Setting up some global variables so that we have a sensible initial state.
 $(document).ready(function() {
   window.conduct = "Intentional";
   window.impact = "Severe";
   window.contact = "High";
   window.goodbloke = 0;
 
+  //#responsive
   if ($(window).width() < 400) {
     $(".labelMed").text("Med");
   }
@@ -11,6 +13,7 @@ $(document).ready(function() {
   checkmrp();
 });
 
+// more #responsive. A more complete way of handling width issues than the onLoad bit
 $(function(){
   $(window).resize(function(){
     if($(this).width() < 400){
@@ -23,40 +26,35 @@ $(function(){
   .resize();//trigger resize on page load
 });
 
+//any time an input is clicked, update our globals accordingly
+//the goodbloke stuff's just a bit of fluff
 $("input").click(function() {
   var $input = $(this);
   if ( $input.attr('name') === "conduct" ) {
-    if ( $input.attr('value') === "Intentional" ) {
-      window.goodbloke++;
-    }
-    else {
-      window.goodbloke = 0;
-    }
     window.conduct = $input.attr('value');
   }
   else if ( $input.attr('name') === "impact" ) {
-    if ( $input.attr('value') === "Severe" ) {
-      window.goodbloke++;
-    }
-    else {
-      window.goodbloke = 0;
-    }
     window.impact = $input.attr('value');
   }
   else if ( $input.attr('name') === "contact" ) {
-    if ( $input.attr('value') === "High" ) {
-      window.goodbloke++;
-    }
-    else {
-      window.goodbloke = 0;
-    }
     window.contact = $input.attr('value');
+  }
+
+  if ( $input.attr('value') === "Intentional" || $input.attr('value') === "Severe" || $input.attr('value') === "High") {
+    window.goodbloke++;
+  }
+  else {
+    window.goodbloke = 0;
   }
 
   checkmrp();
 });
 
+//this is messy, uses a bunch of chaining ifs since there's a pretty limited set
+//there's probably a better way to do it by assigning each option a value and assigning value ranges to numbers of weeks but that seemed a bit over the top for a set of 16 outcomes
+//but the mrp doesn't use points so why should i?
 function checkmrp() {
+  //literally should never happen - legacy sanity check
   if ( window.conduct == null || window.impact == null || window.contact == null ) {
     return;
   }
@@ -68,6 +66,7 @@ function checkmrp() {
     $plea = "Maybe just a fine, I'm sure he's going through a lot.";
     $(".baseSanction").text($base);
     $(".pleaSanction").text($plea);
+    //no point doing the rest, he's a good bloke.
     return;
   }
 
